778a19e2a66bb3df171d227bb020730de478b05e
Lucca Huijgens <lucca.huijgens@student.hu.nl>
2018-06-20 12:58:21 +0200

    Class removed

--- src/main/java/servant/persistence/ServantDao.java
+++ src/main/java/servant/persistence/ServantDao.java
@@ -3,7 +3,6 @@ package servant.persistence;
 import java.util.ArrayList;
 
 import servant.domain.Servant;
-import servant.domain.ServantClass;
 
 public interface ServantDao{
 		ArrayList<Servant> getAllServants();
